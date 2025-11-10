0f2b86968f3d51e2f658c4f028095768c0afbaf0
nghihuynh04 <nghihuynh1210@gmail.com>
2025-11-10 10:38:50 +0700

    refactor: Remove DbConnectionFactory class on cache and update .gitignore for local DB connection

--- DB/DbConnectionFactory.cs
+++ DB/DbConnectionFactory.cs
@@ -4,9 +4,11 @@ namespace mini_supermarket.DB
 {
     internal static class DbConnectionFactory
     {
-        // TODO: Move to configuration if needed.
-        // private const string ConnectionString = "Data Source=.\\sqlexpress;Initial Catalog=mini_sp;Integrated Security=True;Encrypt=False";
-        private const string ConnectionString = "Data Source=.\\sqlexpress;Initial Catalog=mini_sp;Integrated Security=True;Encrypt=False";
+
+    // TODO: Move to configuration if needed.
+    // private const string ConnectionString = "Data Source=.\\sqlexpress;Initial Catalog=mini_sp;Integrated Security=True;Encrypt=False";
+
+        private const string ConnectionString = "Data Source=DESKTOP-CNHTUEA;Initial Catalog=mini_sp;Integrated Security=True;Encrypt=False";
 
         internal static SqlConnection CreateConnection()
         {
